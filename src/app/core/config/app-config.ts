aed3dd99d38e3dd737995eda62b248bbfc0ceaa5
KingSkrip <skrip5025@gmail.com>
2026-01-08 11:25:01 -0600

    APP-CONFIG DE preparacion para servidor

--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -1,8 +1,10 @@
 import { AppConfig } from './app-config.model';
 
 export const APP_CONFIG: AppConfig = {
-  apiUrl: 'http://localhost:8000/api/',
-  apiBase:'http://localhost:8000',
+  // apiUrl: 'http://localhost:8000/api/',
+  // apiBase:'http://localhost:8000',
+  apiUrl: 'https://fibrasan.ddns.net/api/public/api/',
+  apiBase:'https://fibrasan.ddns.net/api/public',
   appName: 'Fibrasan',
   environment: 'dev',
   featureFlagX: true,
