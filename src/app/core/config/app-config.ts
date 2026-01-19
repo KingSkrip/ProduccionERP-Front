ac053c2f7c63a1448299125e3dd46043fa73a365
KingSkrip <skrip5025@gmail.com>
2026-01-19 08:56:11 -0600

    cambios para servidor

--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -3,12 +3,12 @@ import { AppConfig } from './app-config.model';
 export const APP_CONFIG: AppConfig = {
 
   //LOCAL
-  apiUrl: 'http://localhost:8000/api/',
-  apiBase:'http://localhost:8000',
+  // apiUrl: 'http://localhost:8000/api/',
+  // apiBase:'http://localhost:8000',
 
   //SERVIDOR
-  // apiUrl: 'https://fibrasan.ddns.net/api/public/api/',
-  // apiBase: 'https://fibrasan.ddns.net/api/public',
+  apiUrl: 'https://fibrasan.ddns.net/api/public/api/',
+  apiBase: 'https://fibrasan.ddns.net/api/public',
 
   //TUNNEL
   // apiUrl: 'https://duplicate-appliances-commit-stage.trycloudflare.com/api/',
