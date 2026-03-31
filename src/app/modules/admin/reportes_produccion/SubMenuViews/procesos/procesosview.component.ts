59501c4a2c4860102413831e88ed843d3344f776
skrip5025@gmail.com <tuemail@dominio.com>
2026-03-31 11:58:39 -0600

    modificacion de diseño para  procesos en movil

--- src/app/modules/admin/reportes_produccion/SubMenuViews/procesos/procesosview.component.ts
+++ src/app/modules/admin/reportes_produccion/SubMenuViews/procesos/procesosview.component.ts
@@ -2,36 +2,44 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { ProcesosTabComponent } from '../../list/tabs/procesos/procesos-tab.compoonent';
-import { PorRevisarTabComponent } from '../../list/tabs/por revisar/porrevisar-tab.component';
-import { SaldosTabComponent } from '../../list/tabs/saldos/saldos.component';
 import { EmbarquesTabComponent } from '../../list/tabs/embarques/embarques.component';
+import { PorRevisarTabComponent } from '../../list/tabs/por revisar/porrevisar-tab.component';
+import { ProcesosTabComponent } from '../../list/tabs/procesos/procesos-tab.compoonent';
+import { SaldosTabComponent } from '../../list/tabs/saldos/saldos.component';
 
 @Component({
-    selector: 'Procesos-view',
-    templateUrl: './procesosview.component.html',
-    styleUrls: ['./procesosview.component.scss'],
-    standalone: true,
-    imports: [
-        CommonModule,
-        MatIconModule,
-        MatTabsModule,
-        ProcesosTabComponent,
-        PorRevisarTabComponent,
-        SaldosTabComponent,
-        EmbarquesTabComponent
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'Procesos-view',
+  templateUrl: './procesosview.component.html',
+  styleUrls: ['./procesosview.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatTabsModule,
+    ProcesosTabComponent,
+    PorRevisarTabComponent,
+    SaldosTabComponent,
+    EmbarquesTabComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProcesosViewComponent {
-    selectedTabIndex = 0;
+  selectedTabIndex = 0;
+  constructor() {}
 
-    constructor() {
-       
-    }
+  onTabChange(index: number): void {
+    this.selectedTabIndex = index;
+    // console.log('Tab cambiado a:', index);
+  }
 
-    onTabChange(index: number): void {
-        this.selectedTabIndex = index;
-        // console.log('Tab cambiado a:', index);
-    }
+
+
+  // Método que se llama al hacer clic en los tabs de móvil
+  selectTab(index: number): void {
+    this.selectedTabIndex = index;
+    
+    // Opcional: Guardar la pestaña seleccionada para recordarla
+    // localStorage.setItem('lastTab', index.toString());
+  }
+
 }
