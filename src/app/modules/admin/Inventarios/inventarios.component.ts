850960a8d621ab1db68befebe0a2bf09762a7520
skrip5025@gmail.com <tuemail@dominio.com>
2026-08-19 11:43:30 -0600

    .

--- src/app/modules/admin/Inventarios/inventarios.component.ts
+++ src/app/modules/admin/Inventarios/inventarios.component.ts
@@ -19,8 +19,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSelectModule } from '@angular/material/select';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { BarcodeFormat } from '@zxing/library';
-import { ZXingScannerModule, ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 type Seccion = 'general' | 'rollos';
 
